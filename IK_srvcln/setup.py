b5fd7e1281a8efc439cc033b6e2906048232f08e
ZhunC <95588119+ZhunC@users.noreply.github.com>
2023-11-26 12:16:42 -0500

    This is the half finished IK node
    Core computation functions are done and annotated. The server-client structure is still unclear to me and I copied the basic controller code there for now.
    A client requesting to the SetJointPosition and an IK server in the same node? or 2 nodes in the same file? Or 2 nodes in 2 files?

---
+++ IK_srvcln/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+package_name = 'IK_srvcln'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='zcheng',
+    maintainer_email='zcheng2@wpi.edu',
+    description='RBE 500 IK server and client',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
